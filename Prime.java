52f4088b5ca38b5f2493dc7caa9d4fc64e8e80f6
Sambid009 <66829721+Sambid009@users.noreply.github.com>
2024-05-24 22:22:48 +0545

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,36 @@
+
+package unit1;
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        int n1,n2, count=0;
+        System.out.println("Enter any number..");
+        Scanner s1 = new Scanner(System.in);
+        n1= s1.nextInt();
+        
+         System.out.println("Enter any number..");
+        Scanner s2 = new Scanner(System.in);
+        n2= s2.nextInt();
+        
+       
+        
+        
+        
+    }
+}
+  //-------------------------------------------------Prime or not for 1input number--------
+//        for(int i=1; i<=n1; i++){
+//        if(n1%i==0){
+//            count++;
+//        }
+//    }
+//        if(count==2){
+//            System.out.println("Prime num");
+//        }
+//        else{
+//            System.out.println("Not Prime");
+//        }
+//    }
+    
+
